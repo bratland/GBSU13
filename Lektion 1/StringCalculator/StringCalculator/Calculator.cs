dd9ad500fa8f6d046563cb305a8305082d9edf87
Anders Bratland <anders@bratland.se>
2014-10-20 15:24:29 +0200

    String Calculator Kata

---
+++ Lektion 1/StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString))
+                return 0;
+
+            var validDelimiters = new List<string> { ",", "\n" };
+            var delimiter = FindDelimiters(inputString);
+            inputString = CleanInput(inputString);
+            if (delimiter != null)
+                validDelimiters.AddRange(delimiter);
+
+            var stringValues = inputString.Split(validDelimiters.ToArray(), StringSplitOptions.None);
+            var ints = stringValues.Select(int.Parse);
+
+
+            
+            try
+            {
+                var sum = ints.Where(v => v <= 1000).Sum();
+                if (ints.Any(i => i < 0))
+                {
+                    throw new Exception(" Illegal Values: " + string.Join(", ", ints.Where(i => i < 0)));
+                }                
+                return sum;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Contains invalid chars");
+            }
+        }
+
+        private string CleanInput(string inputString)
+        {
+            if (inputString.StartsWith("//"))
+                inputString = inputString.Split(new char[] { '\n' }, 2)[1];
+
+            inputString = inputString.Replace(" ", "");
+
+            return inputString;
+        }
+
+        private IEnumerable<string> FindDelimiters(string inputString)
+        {
+            if (inputString.StartsWith("//"))
+            {
+                var rawDelimiters = inputString.Substring(2);
+                var delimiters = new List<string>();
+
+                if (rawDelimiters[0] != '[')
+                    delimiters.Add(rawDelimiters.Split('\n')[0]);
+
+                while (rawDelimiters.Contains('['))
+                {
+                    var splittedDelimiters = rawDelimiters.Split(new char[] { ']' }, 2);
+                    delimiters.Add(splittedDelimiters[0].Substring(1));
+                    rawDelimiters = splittedDelimiters[1];
+                }
+
+                return delimiters;
+            }
+
+            return new string[]{};
+        }
+    }
+}
---
+++ Lektion 1/StringCalculator/StringCalculatorTest/UnitTest1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TryCreateCalculator()
+        {
+            var calc = new Calculator();
+        }
+
+        [TestMethod]
+        public void TryAddEmptyString()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("");
+
+            // Assert
+            Assert.AreEqual(0, result, "Empty string should return 0");
+        }
+
+        [TestMethod]
+        public void TryAddNumber()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("5");
+
+            // Assert
+            Assert.AreEqual(5, result, "Result should be 5");
+        }
+
+        [TestMethod]
+        public void TryAddListOfNumbers()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("1, 2, 3, 4, 5");
+
+            // Assert
+            Assert.AreEqual(15, result, "Result should be 15");
+        }
+
+        [TestMethod]
+        public void TryAddNumbersWithNewLines()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("1\n2,3");
+
+            // Assert
+            Assert.AreEqual(6, result, "Result should be 6");
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TryNotAllowedSyntaxOfNumbers()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("1,\n");
+
+            // Assert
+            Assert.AreEqual(0, result, "Result should be 0");
+        }
+
+        [TestMethod]
+        public void TryDelimeterSyntax()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("//;\n1;2");
+
+            // Assert
+            Assert.AreEqual(3, result, "Result should be 3");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TrySubracting()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("1, 2, -3, -4");
+
+        }
+
+        [TestMethod]
+        public void TryIgnoringNumbersAboveThousand()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("1, 2, 1001, 1002");
+
+            // Assert
+            Assert.AreEqual(3, result, "Result should be 3");
+        }
+
+        [TestMethod]
+        public void AllowDelimitersWithMultipleChars()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("//[***]\n1***2***3");
+
+            // Assert
+            Assert.AreEqual(6, result, "Result should be 6");
+        }
+
+        [TestMethod]
+        public void TryIgnoringMultipleDelimeters()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Add("//[*][%]\n1*2%3");
+
+            // Assert
+            Assert.AreEqual(6, result, "Result should be 6");
+        }
+    }
+}
